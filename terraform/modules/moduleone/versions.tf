c7a04c7eaae957b375a35c2cd6f68c0c66e23c69
bgdanix <bogdalecu@gmail.com>
2020-11-19 12:02:24 +0200

    Update versions.tf

--- terraform/modules/moduleone/versions.tf
+++ terraform/modules/moduleone/versions.tf
@@ -15,9 +15,16 @@
  */
 
 terraform {
-  required_version = ">= 0.12.6"
+  required_version = ">= 0.13.0"
+
   required_providers {
-    google      = ">= 3.32, <4.0.0"
-    google-beta = ">= 3.32, <4.0.0"
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.34.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.34.0"
+    }
   }
 }
